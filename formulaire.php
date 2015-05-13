0cbc01be526f79f19afbee92ccd11ff36b3414bd
julaiii <bonnet.julie@hotmail.fr>
2015-05-13 15:08:55 +0200

    Initial Commit

---
+++ formulaire.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+<title>VIP</title>
+  <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+</head>
+
+  <body>
+    <h1>Réserve ta soirée : I'm dancing with myself #party</h1>
+    <p>Entrée: 35€</p>
+
+<form action="secret.php" method="post" class="form-horizontal" style="margin-top:100px; margin-left:25px;">
+  <p><h2>Gagne 1 place VIP en répondant à cette charade :</h2> <br>
+  Mon premier se met en place lors d'une manoeuvre militaire.<br>
+perdre mon deuxième est le comble du dégustateur.<br>
+Mon troisième:la voiture en a quatre.<br>
+Mon tout est originaire d'Australie.<br>
+<label for="password">Qui suis-je?</p></label><br>
+    <input id="password" name="password" placeholder="mot de passe" type="password" style="">
+</form>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  	
+  </body>
+  </html>
+
---
+++ secret.php
@@ -0,0 +1,37 @@
+<?php if($_POST["password"] == "kangourou"){
+    echo "<h1>Bravo petit "; echo $_POST['password']; echo "! Tu as gagné une place VIP !!</h1>"
+ ;}
+    else {
+     Header("Location: formulaire.php");
+  		}
+	
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+<title>VIP</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+</head>
+<body>
+<p>Hiiiiiiihouuuuuu !!!!</p>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+</body>
+</html>
